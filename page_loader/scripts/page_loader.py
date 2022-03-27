50b6a31f32c613a8ac220db36fa54881c30229d4
emp7yhead <artyomkropp@gmail.com>
2022-03-27 15:48:28 +0900

    Add: logging in main script.

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,13 +1,25 @@
 """Page-loader main script."""
 
-from page_loader.cli import cli
-from page_loader.loader import loader
+import logging
+import sys
+
+from page_loader.cli.cli import parse_arguments
+from page_loader.loader.loader import download
+from page_loader.logger.logger_setup import setup_logging
+from page_loader.logger.exceptions import NetworkError, FileSystemError
+
+logger = logging.getLogger(__name__)
+setup_logging()
 
 
 def main() -> None:
     """Run page-loader."""
-    dir_path, url = cli.parse_arguments()
-    loader.download(url, dir_path)
+    url, dir_path = parse_arguments()
+    try:
+        download(url, dir_path)
+    except (NetworkError, FileSystemError):
+        sys.exit(1)
+    logger.info('Page was successfully downloaded into {0}'. format(dir_path))
 
 
 if __name__ == '__main__':
