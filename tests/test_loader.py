8b3bb6474762d1431306b243d292fe34bf7d584a
emp7yhead <artyomkropp@gmail.com>
2022-03-27 23:18:47 +0900

    Tests: about raised exceptions

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2,9 +2,11 @@
 
 import os
 import tempfile
-
+import pytest
 import requests
+import requests_mock
 from page_loader.loader.loader import download
+from page_loader.logger.exceptions import FileSystemError, NetworkError
 
 TEST_URL = 'http://test.com'
 
@@ -23,3 +25,27 @@ def test_loader(requests_mock):
         with open(page_path, 'r') as test_file:
             assert test_file.read() == requests.get(TEST_URL).text
     assert expected_path == page_path
+
+
+def test_download_io_error(requests_mock):
+    """Test raising FileSystemError.
+
+    Args:
+        requests_mock: for mocking test url.
+    """
+    requests_mock.get(TEST_URL, text='<html>\n</html>')
+    with pytest.raises(FileSystemError):
+        download(TEST_URL, '/directory/doesnt/exist')
+
+
+def test_download_network_error():
+    """Test raising NetworkError.
+
+    Args:
+        requests_mock: for mocking test url.
+    """
+    with requests_mock.Mocker() as mock:
+        mock.get(TEST_URL, status_code=404)
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            with pytest.raises(NetworkError):
+                download(TEST_URL, tmpdirname)
