06e4cf8668e7629777494f9ab1131bd26377b2dc
emp7yhead <artyomkropp@gmail.com>
2022-03-27 22:25:37 +0900

    Fix: delete logging from main script

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,6 +1,4 @@
 """Page-loader main script."""
-
-import logging
 import sys
 
 from page_loader.cli.cli import parse_arguments
@@ -8,9 +6,6 @@ from page_loader.loader.loader import download
 from page_loader.logger.exceptions import FileSystemError, NetworkError
 from page_loader.logger.logger_setup import setup_logging
 
-SUCCESS_DOWNLOAD_MSG = 'Page was successfully downloaded into {0}'
-
-logger = logging.getLogger(__name__)
 setup_logging()
 
 
@@ -21,7 +16,6 @@ def main() -> None:
         download(url, dir_path)
     except (NetworkError, FileSystemError):
         sys.exit(1)
-    logger.info(SUCCESS_DOWNLOAD_MSG.format(dir_path))
 
 
 if __name__ == '__main__':
